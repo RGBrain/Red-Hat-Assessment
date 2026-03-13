ef9a43b7bd78be4a0bc85fb1699c839a4f4f9517
Matt <matt@brainweb.dev>
2026-03-13 10:14:55 +0000

    SectionThree - code mobile layout

--- src/components/FeatureList.tsx
+++ src/components/FeatureList.tsx
@@ -5,33 +5,41 @@
  * i.e. it takes the list data as prop and displays it in a three-column layout
  ********************/
 
-import React from 'react'
+import React from "react";
 
 type FeatureItem = {
-  title: string
-  description: string
-  titleClass?: string
-  descriptionClass?: string
-  icon?: React.ElementType
-  iconClass?: string
-  iconSize?: number
-}
+  title: string;
+  description: string;
+  titleClass?: string;
+  descriptionClass?: string;
+  icon?: React.ElementType;
+  iconClass?: string;
+  iconSize?: number;
+};
 
 const FeatureList: React.FC<{ listData: FeatureItem[] }> = ({ listData }) => {
   return (
     <div className="mx-auto mt-0 flex max-w-[1400px] flex-col space-y-24 md:flex-row md:flex-wrap md:justify-center md:space-y-0 md:gap-x-20 md:gap-y-30 xl:gap-x-16">
       {listData.map(
         (
-          { title, description, titleClass, descriptionClass, icon: Icon, iconClass, iconSize },
+          {
+            title,
+            description,
+            titleClass,
+            descriptionClass,
+            icon: Icon,
+            iconClass,
+            iconSize,
+          },
           idx,
         ) => (
           <div
             key={title ?? idx}
-            className={`max-w-[300px] text-center text-lg min-[1400px]:max-w-[400px] md:mx-auto md:w-1/2 md:px-4 xl:w-1/3 xl:px-2 ${titleClass ?? ''}`}
+            className={`max-w-[300px] text-center text-lg min-[1400px]:max-w-[400px] md:mx-auto md:w-1/2 md:px-4 xl:w-1/3 xl:px-2 ${titleClass ?? ""}`}
           >
             {Icon && (
               <div
-                className={`mx-auto ${iconClass ?? ''}`}
+                className={`mx-auto ${iconClass ?? ""}`}
                 style={{ fontSize: iconSize ?? undefined }}
               >
                 <Icon className={iconClass} size={iconSize} strokeWidth={0.8} />
@@ -41,7 +49,7 @@ const FeatureList: React.FC<{ listData: FeatureItem[] }> = ({ listData }) => {
               {title}
             </h2>
             <p
-              className={`mt-3 text-[1.10rem] leading-6 text-black md:text-base md:leading-6 xl:px-4 xl:text-[1.20rem] xl:leading-[1.85rem] xl:font-normal ${descriptionClass ?? ''}`}
+              className={`mt-3 text-[1.10rem] leading-6 font-normal text-black md:text-base md:leading-6 xl:px-4 xl:text-[1.20rem] xl:leading-[1.85rem] xl:font-normal ${descriptionClass ?? ""}`}
             >
               {description}
             </p>
@@ -49,7 +57,7 @@ const FeatureList: React.FC<{ listData: FeatureItem[] }> = ({ listData }) => {
         ),
       )}
     </div>
-  )
-}
+  );
+};
 
-export default FeatureList
+export default FeatureList;
