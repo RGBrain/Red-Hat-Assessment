ca930ddfc4611063db8929a175ed04928e8733a0
Matt <matt@brainweb.dev>
2026-03-13 09:58:33 +0000

    SectionOne - code mobile layout

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -10,21 +10,21 @@ import Image from 'next/image'
 const SectionOne = () => {
   return (
     <section id="s1" className="py-20">
-      <div className="w-full mx-auto flex justify-evenly items-center gap-10 max-w-[1400px]">
+      <div className="w-full mx-auto flex flex-col-reverse lg:flex-row justify-evenly items-start lg:items-center gap-10 max-w-[1400px] pl-8 lg:pl-0">
         {/* ADDED A MAX-WIDTH SO THE SECTION IS CROPPED ON VERY WIDE SCREENS */}
         <Image
           src="/red-hat-server.png"
           alt="Red Hat Server"
           width={500}
           height={500}
-          className="w-full flex-1"
+          className="w-full flex-1 -ml-6 lg:ml-0"
         />
         <div className="flex-1 flex flex-col gap-10">
           {/* THIS DIV IS ONE FLEX ITEM (used as a container for the two Paragraph elements) */}
-          <p className="text-7xl leading-snug font-bold">
+          <p className="text-4xl lg:text-7xl leading-snug font-bold">
             Unlocking the Value of Your Linux Estate
           </p>
-          <p className="text-4xl font-semibold">A Red Hat Assessment Deep Dive</p>
+          <p className="text-xl lg:text-4xl font-semibold">A Red Hat Assessment Deep Dive</p>
         </div>
       </div>
     </section>
