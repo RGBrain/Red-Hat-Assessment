9e6506d309f6707494f3ae16aaf8531e1e7b10e3
Matt <matt@brainweb.dev>
2026-03-13 10:54:02 +0000

    Footer - code mobile layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,14 +6,14 @@
 
 const Footer = () => {
   return (
-    <footer className="text-white bg-black py-6">
-      <div className="container mx-auto px-4 flex justify-between gap-20 lg:gap-60 xl:gap-100">
-        <div className="text-sm flex flex-col gap-3 flex-1">
-          <p>
-            Curated by LeadaMarket for the benefit of Red Hat Business Partners who are clients of
-            TD SYNNEX.
+    <footer className="bg-black py-6 text-white">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-12 px-4 lg:flex-row lg:items-start lg:gap-60 xl:gap-100">
+        <div className="mx-auto flex w-fit max-w-prose flex-1 flex-col gap-6 text-left text-sm lg:gap-3">
+          <p className="mx-auto w-fit max-w-prose text-left sm:mx-0">
+            Curated by LeadaMarket for the benefit of Red Hat Business Partners
+            who are clients of TD SYNNEX.
           </p>
-          <p>
+          <p className="mx-auto w-fit max-w-prose lg:mx-0">
             <a
               href="https://leadamarket.com/trading-terms/?__hstc=247219483.897421ff35bdda3f46639e3f3a90a1f4.1763634553011.1773251158080.1773320784345.9&__hssc=247219483.1.1773320784345&__hsfp=adebdb7cb5be95e8b1209c98d6268819"
               target="_blank"
@@ -24,16 +24,17 @@ const Footer = () => {
             </a>
           </p>
         </div>
-        <div className="text-sm flex-1">
+        <div className="flex-1 text-sm">
           <p>
-            © TD SYNNEX Corporation, 2026. TD SYNNEX and TD SYNNEX logo are registered trademarks of
-            TD SYNNEX Corporation in the United States and other countries. All other trademarks are
-            the property of their respective owners.
+            © TD SYNNEX Corporation, 2026. TD SYNNEX and TD SYNNEX logo are
+            registered trademarks of TD SYNNEX Corporation in the United States
+            and other countries. All other trademarks are the property of their
+            respective owners.
           </p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
