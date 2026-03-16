e0536f0665385aed5c0c3cf371a55b39e186ffeb
Matt <matt@brainweb.dev>
2026-03-16 10:42:39 +0000

    SectionOne text headings - add right padding for desktops

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -4,31 +4,33 @@
  * This section has the Red Hat Server Laptop image on the left
  ********************/
 
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const SectionOne = () => {
   return (
     <section id="s1" className="py-20">
-      <div className="w-full mx-auto flex flex-col-reverse lg:flex-row justify-evenly items-start lg:items-center gap-10 max-w-[1400px] pl-8 lg:pl-0">
+      <div className="mx-auto flex w-full max-w-[1400px] flex-col-reverse items-start justify-evenly gap-10 pl-8 lg:flex-row lg:items-center lg:pl-0">
         {/* ADDED A MAX-WIDTH SO THE SECTION IS CROPPED ON VERY WIDE SCREENS */}
         <Image
           src="/red-hat-server.png"
           alt="Red Hat Server"
           width={500}
           height={500}
-          className="w-full flex-1 -ml-6 lg:ml-0"
+          className="-ml-6 w-full flex-1 lg:ml-0"
         />
-        <div className="flex-1 flex flex-col gap-10">
+        <div className="flex flex-1 flex-col gap-10 lg:pr-5">
           {/* Div for title and subtitle */}
-          <p className="text-4xl lg:text-7xl leading-snug font-bold">
+          <p className="text-4xl leading-snug font-bold lg:text-7xl">
             Unlocking the Value of Your Linux Estate
           </p>
-          <p className="text-xl lg:text-4xl font-semibold">A Red Hat Assessment Deep Dive</p>
+          <p className="text-xl font-semibold lg:text-4xl">
+            A Red Hat Assessment Deep Dive
+          </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SectionOne
+export default SectionOne;
