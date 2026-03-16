1b4bc842101243e3902529d5e7347dadf58af6f1
Matt <matt@brainweb.dev>
2026-03-16 07:25:22 +0000

    fadeIn SectionFive when in viewport

--- src/components/SectionFive.tsx
+++ src/components/SectionFive.tsx
@@ -9,6 +9,7 @@
 import React from "react";
 import { Mail } from "lucide-react";
 import posthog from "posthog-js";
+import FadeTextWrapper from "./FadeTextWrapper";
 
 const SectionFive = () => {
   const handleEmailClick = () => {
@@ -20,31 +21,33 @@ const SectionFive = () => {
   return (
     <section id="s5" className="py-8">
       <div className="mx-auto flex max-w-[1350px] flex-col items-center">
-        <h2 className="text-red-hat-red mx-auto mb-6 text-center text-3xl font-normal lg:mb-10">
-          Get in Touch
-        </h2>
-        <Mail size={70} strokeWidth={0.8} className="text-red-hat-red mb-4" />
+        <FadeTextWrapper>
+          <h2 className="text-red-hat-red mx-auto mb-6 text-center text-3xl font-normal lg:mb-10">
+            Get in Touch
+          </h2>
+          <Mail size={70} strokeWidth={0.8} className="text-red-hat-red mb-4" />
 
-        <p className="mx-auto mt-2 mb-4 text-center text-lg">
-          <a
-            href="mailto:estate-assessment@businesstech.tech"
-            className="text-red-hat-red underline"
-            onClick={handleEmailClick}
-          >
-            estate-assessment@businesstech.tech
-          </a>
-        </p>
+          <p className="mx-auto mt-2 mb-4 text-center text-lg">
+            <a
+              href="mailto:estate-assessment@businesstech.tech"
+              className="text-red-hat-red underline"
+              onClick={handleEmailClick}
+            >
+              estate-assessment@businesstech.tech
+            </a>
+          </p>
 
-        <div className="mt-8 w-full">
-          {/* LOGOS */}
-          <div className="flex w-full justify-center lg:justify-end">
-            <img
-              src="/logos-td-synnex-and-red-hat.png"
-              alt="TD Synnex and Red Hat"
-              className="h-12 object-contain md:h-16 lg:pr-10"
-            />
+          <div className="mt-8 w-full">
+            {/* LOGOS */}
+            <div className="flex w-full justify-center lg:justify-end">
+              <img
+                src="/logos-td-synnex-and-red-hat.png"
+                alt="TD Synnex and Red Hat"
+                className="h-12 object-contain md:h-16 lg:pr-10"
+              />
+            </div>
           </div>
-        </div>
+        </FadeTextWrapper>
       </div>
     </section>
   );
