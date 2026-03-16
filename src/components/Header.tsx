bb28d00796d9ea2b05ed6811c874ea1f742814ab
Matt <matt@brainweb.dev>
2026-03-16 12:38:08 +0000

    Header - add bottom shadow

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import BtnRegister from "@/components/BtnRegister";
 
 const Header = () => {
   return (
-    <header className="bg-red-hat-red sticky top-0 z-50 w-full py-6 text-white transition duration-750 ease-in-out">
+    <header className="bg-red-hat-red sticky top-0 z-50 w-full py-6 text-white shadow-xl transition duration-750 ease-in-out">
       <div className="container mx-auto flex justify-end px-4">
         <BtnRegister />
       </div>
